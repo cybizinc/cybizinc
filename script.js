2d8135418f52b766edca62cedfc26d480c829ba5
cybizinc <cybizinc@gmail.com>
2026-04-17 23:54:00 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const services = ['Consulting', 'Design', 'Development'];
-  const ul = document.getElementById('services');
-  services.forEach(service => {
-    const li = document.createElement('li');
-    li.textContent = service;
-    ul.appendChild(li);
-  });
-});   
